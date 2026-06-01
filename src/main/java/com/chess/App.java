6baea410e457ba716b5b2a18874dcd147caf166a
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-06-01 21:14:48 +0000

    Test new table class.

--- src/main/java/com/chess/App.java
+++ src/main/java/com/chess/App.java
@@ -3,16 +3,18 @@ package com.chess;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import com.chess.model.Table;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 
 import com.chess.http.handler.ChessHandler;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 50);
+    public static void main(String[] args) throws IOException {/*        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 50);
         server.createContext("/api/chess", new ChessHandler());
         server.start();
-        System.out.println("Server started on port 8080");
+        System.out.println("Server started on port 8080");*/
+        Table table = new Table();
+        table.move(1, 1, 3, 1);
     }
 }
