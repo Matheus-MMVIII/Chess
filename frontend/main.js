2ad39d4d111926f365af5fac88b29dfcc3441111
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-06-04 17:27:40 +0000

    Using the API in frontend to view the board.

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,8 @@
 async function loadChess() {
     const url = "http://localhost:8080/api/chess/";
+    table = document.getElementById("table");
+    let tableText = ``;
+
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -8,6 +11,15 @@ async function loadChess() {
 
         const result = await response.json();
         console.log(result);
+
+        for (i = 1; i <= 8; i++) {
+            tableText += `<div class="line">`;
+            for (j = 0; j <= 7; j++) {
+                tableText += `<a class="piece">${result["line" + i][j]}</a>`;
+            }
+            tableText += `</div>`;
+        }
+        table.innerHTML = tableText;
     } catch (error) {
         console.error(error.message);
     }
