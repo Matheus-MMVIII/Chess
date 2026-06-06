4125ea1f0f7392175ded9f52d66a31dfb584fb13
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-06-06 02:33:21 +0000

    Add images to pieces.

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,19 @@
+const pieces = {
+    r: ".images/white-rook.png",
+    h: "images/white-horse.png",
+    b: "images/white-bishop.png",
+    q: "images/white-queen.png",
+    k: "images/white-king.png",
+    p: "images/white-pawn.png",
+
+    R: "images/black-rook.png",
+    H: "images/black-horse.png",
+    B: "images/black-bishop.png",
+    Q: "images/black-queen.png",
+    K: "images/black-king.png",
+    P: "images/black-pawn.png"
+};
+
 async function loadChess() {
     const url = "http://localhost:8080/api/chess/";
     table = document.getElementById("table");
@@ -16,16 +32,30 @@ async function loadChess() {
             if (i % 2 === 0) {
                 for (j = 0; j <= 7; j++) {
                     if (j % 2 === 0) {
-                        tableText += `<a class="piece-back-white">${result["line" + i][j]}</a>`;
-                    } else
-                        tableText += `<a class="piece-back-black">${result["line" + i][j]}</a>`;
+                        if (result["line" + i][j] === ".")
+                            tableText += `<div class="piece-back-white"></div>`;
+                        else
+                            tableText += `<div class="piece-back-white"><img src="${pieces[result["line" + i][j]]}"></div>`;
+                    } else {
+                        if (result["line" + i][j] === ".")
+                            tableText += `<div class="piece-back-black"></div>`;
+                        else
+                            tableText += `<div class="piece-back-black"><img src="${pieces[result["line" + i][j]]}"></div>`;
+                    }
                 }
             }else {
                 for (j = 0; j <= 7; j++) {
                     if (j % 2 !== 0) {
-                        tableText += `<a class="piece-back-white">${result["line" + i][j]}</a>`;
-                    } else
-                        tableText += `<a class="piece-back-black">${result["line" + i][j]}</a>`;
+                        if (result["line" + i][j] === ".")
+                            tableText += `<div class="piece-back-white"></div>`;
+                        else
+                            tableText += `<div class="piece-back-white"><img src="${pieces[result["line" + i][j]]}"></div>`;
+                    } else {
+                        if (result["line" + i][j] === ".")
+                            tableText += `<div class="piece-back-black"></div>`;
+                        else
+                            tableText += `<div class="piece-back-black"><img src="${pieces[result["line" + i][j]]}"></div>`;
+                    }
                 }
             }
         }
