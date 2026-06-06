a334ae84d387d9917cf416776fe14647f47f2f45
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-06-06 21:35:03 +0000

    Show the routes in backend.

--- src/main/java/com/chess/App.java
+++ src/main/java/com/chess/App.java
@@ -3,19 +3,20 @@ package com.chess;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-import com.chess.http.handler.ImageHandler;
+import com.chess.http.handler.*;
 import com.chess.model.Table;
 import com.chess.service.ChessService;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 
-import com.chess.http.handler.ChessHandler;
-
 public class App {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 50);
+        server.createContext("/", new FrontendHandler());
         server.createContext("/api/chess", new ChessHandler(new ChessService()));
         server.createContext("/images", new ImageHandler());
+        server.createContext("/style.css", new CssHandler());
+        server.createContext("/script.js", new JsHandler());
         server.start();
         System.out.println("Server started on port 8080");
         /*
