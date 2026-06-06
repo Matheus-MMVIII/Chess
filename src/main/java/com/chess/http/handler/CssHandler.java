64321e2006f453b2050f1daadda20e404bf562a8
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-06-06 21:31:41 +0000

    Create a acess to css.

---
+++ src/main/java/com/chess/http/handler/CssHandler.java
@@ -0,0 +1,28 @@
+package com.chess.http.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CssHandler extends BaseHandler {
+
+    public CssHandler() {
+
+    }
+
+    @Override
+    public void handleRequest(HttpExchange exchange) throws IOException {
+
+        Path file = Path.of("frontend/style.css");
+
+        byte[] bytes = Files.readAllBytes(file);
+
+        exchange.getResponseHeaders().set("Content-Type", "text/css");
+        exchange.sendResponseHeaders(200, bytes.length);
+
+        exchange.getResponseBody().write(bytes);
+        exchange.getResponseBody().close();
+    }
+}
